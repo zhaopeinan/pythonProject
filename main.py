7aafefccf8f08e9ae8a93a6044c4a630a683bc94
PN ZHAO <pnzhao@bjtu.edu.cn>
2023-07-12 18:38:06 +0800

    jupyter init

--- main.py
+++
@@ -1,9 +0,0 @@
-import torch as t
-import torch.nn as nn
-from torch.autograd import Variable as V
-input = V(t.randn(2,3))
-model = nn.Linear(3,4)
-output1 = model(input) # 得到输出方式1
-output2 = nn.functional.linear(input, model.weight, model.bias) # 得到输出方式2
-print(output1 == output2)
-# test git
